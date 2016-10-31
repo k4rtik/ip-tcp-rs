1e20886b48badaed7c56db7f85e31252c44db214
Kartik Singhal <kartik@brown.edu>
2016-10-31 19:51:34 -0400

    Dummy TCP stub implementation

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,5 +1,15 @@
 use std::net::Ipv4Addr;
 
+pub struct Socket {
+    // socket  local-addr      port            dst-addr        port    status
+}
+
+struct TCP {
+    // container of TCBs
+}
+
+impl TCP {}
+
 struct TCB {
     local_ip: Ipv4Addr,
     local_port: u16,
@@ -8,31 +18,35 @@ struct TCB {
     tcp_state: u16,
 }
 
-pub fn v_socket() -> i32 {
-    debug!("Creating socket...");
-    debug!("Initializing TCB...");
-    let mut tcb = TCB {
-        local_ip: "0.0.0.0".parse::<Ipv4Addr>().unwrap(),
-        local_port: 0,
-        dst_ip: "0.0.0.0".parse::<Ipv4Addr>().unwrap(),
-        dst_port: 0,
-        tcp_state: 0,
-    };
-    1
-}
+impl TCB {
+    // new()
 
-pub fn v_bind(socket: i32, addr: Ipv4Addr, port: u16) -> i32 {
-    0
-}
+    pub fn v_socket() -> i32 {
+        debug!("Creating socket...");
+        debug!("Initializing TCB...");
+        let mut tcb = TCB {
+            local_ip: "0.0.0.0".parse::<Ipv4Addr>().unwrap(),
+            local_port: 0,
+            dst_ip: "0.0.0.0".parse::<Ipv4Addr>().unwrap(),
+            dst_port: 0,
+            tcp_state: 0,
+        };
+        1
+    }
 
-pub fn v_listen(socket: i32) -> u32 {
-	0
-}
+    pub fn v_bind(socket: i32, addr: Ipv4Addr, port: u16) -> i32 {
+        0
+    }
 
-pub fn v_connect(socket: i32, addr: Ipv4Addr, port: u16) -> i32 {
-	0
-}
+    pub fn v_listen(socket: i32) -> u32 {
+        0
+    }
 
-pub fn v_accept(socket: i32, addr: Ipv4Addr) -> i32 {
-	0
+    pub fn v_connect(socket: i32, addr: Ipv4Addr, port: u16) -> i32 {
+        0
+    }
+
+    pub fn v_accept(socket: i32, addr: Ipv4Addr) -> i32 {
+        0
+    }
 }
