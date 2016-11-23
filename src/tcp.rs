85ad046922d3434307a2be55135dac0eae5dbc5c
Kartik Singhal <kartiksinghal@gmail.com>
2016-11-22 19:00:25 -0500

    Partial state machine for Close UserCall

--- src/tcp.rs
+++ src/tcp.rs
@@ -871,7 +871,9 @@ fn conn_state_machine(tcb_ref: Arc<RwLock<TCB>>,
                         let tcb = &mut (*tcb_ref.write().unwrap());
                         #[allow(match_same_arms)]
                         match tcb.state {
-                            Closed => {}
+                            Closed => {
+                                error!("connection does not exist");
+                            }
                             Listen | SynSent | SynRcvd => {}
                             Estab | FinWait1 | FinWait2 => {}
                             CloseWait => {}
@@ -882,15 +884,83 @@ fn conn_state_machine(tcb_ref: Arc<RwLock<TCB>>,
                     }
                     Close => {
                         let tcb = &mut (*tcb_ref.write().unwrap());
-                        #[allow(match_same_arms)]
                         match tcb.state {
-                            Closed => {}
-                            Listen => {}
-                            SynSent => {}
-                            SynRcvd => {}
-                            Estab => {}
-                            FinWait1 | FinWait2 => {}
-                            CloseWait => {}
+                            Closed => {
+                                error!("connection does not exist");
+                            }
+                            Listen => {
+                                // TODO only for  outstanding receives
+                                error!("closing");
+                                tcb.state = Closed;
+                                break; // delete TCB
+                            }
+                            SynSent => {
+                                // TODO only for any queued sends and receives
+                                error!("closing");
+                                tcb.state = Closed;
+                                break; // delete TCB
+                            }
+                            SynRcvd => {
+                                // TODO take action based on if there are pending SENDs
+                            }
+                            Estab => {
+                                // TODO finish SENDs
+                                t_params = TcpParams {
+                                    src_port: tcb.local_port,
+                                    dst_port: tcb.remote_port,
+                                    seq_num: tcb.snd_nxt,
+                                    ack_num: tcb.rcv_nxt,
+                                    flags: TcpFlags::FIN,
+                                    window: tcb.rcv_wnd,
+                                };
+
+                                build_tcp_header(t_params,
+                                                 tcb.local_ip,
+                                                 tcb.remote_ip,
+                                                 None,
+                                                 &mut pkt_buf);
+                                ip_params = ip::IpParams {
+                                    src: tcb.local_ip,
+                                    dst: tcb.remote_ip,
+                                    ..ip_params
+                                };
+
+                                info!("switching to FinWait1 from Estab");
+                                tcb.state = FinWait1;
+
+                                should_send_packet = true;
+                            }
+                            FinWait1 | FinWait2 => {
+                                error!("connection closing");
+                            }
+                            CloseWait => {
+                                // TODO finish SENDs
+                                t_params = TcpParams {
+                                    src_port: tcb.local_port,
+                                    dst_port: tcb.remote_port,
+                                    seq_num: tcb.snd_nxt,
+                                    ack_num: tcb.rcv_nxt,
+                                    flags: TcpFlags::FIN,
+                                    window: tcb.rcv_wnd,
+                                };
+
+                                build_tcp_header(t_params,
+                                                 tcb.local_ip,
+                                                 tcb.remote_ip,
+                                                 None,
+                                                 &mut pkt_buf);
+                                ip_params = ip::IpParams {
+                                    src: tcb.local_ip,
+                                    dst: tcb.remote_ip,
+                                    ..ip_params
+                                };
+
+                                info!("switching to Closing from CloseWait");
+                                tcb.state = Closing;
+
+                                should_send_packet = true;
+                                // send FIN
+                            }
                             _ => {
                                 error!("connection closing");
                             }
